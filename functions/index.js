9374f0cb3e881477f8bc38573cb3bfa4671883fc
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-04-14 01:41:09 +0800

    Save progress

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,40 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+/**
+ * Copyright 2018 Ding! App
+ * based on
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// The reference to the node under "orders".
+const orderRef = functions.database.ref('/orders');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// Triggers whenever there is any change to the "orders" node and its sub-nodes.
+exports.orderChange = orderChange.onWrite((change) => {
+	// Increments or decrements according to whether the order is inserted or deleted.
+	let increment;
+	if (change.after.exists() && !change.before.exists()) {
+		increment = 1;
+	} else if (!change.after.exists() && change.before.exists()) {
+		increment = -1;
+	} else {
+		return null;
+	}
+	
+	return console.log('Counter updated.');
 });
